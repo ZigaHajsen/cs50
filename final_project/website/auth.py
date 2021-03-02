05e29429f8e9f07608e58afaf820272adfca1a48
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-03-02 15:41:00 +0100

    added sign up setup

--- final_project/website/auth.py
+++ final_project/website/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+import re
+
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
@@ -13,6 +15,25 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        regex = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+
+        if not re.search(regex, email):
+            flash("Email must be valid.", category="error")
+        elif len(firstName) < 2:
+            flash("Name to short.", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match.", category="error")
+        elif len(password1) < 6:
+            flash("Password must be at least 6 characters.", category="error")
+        else:
+            flash("Account created!", category="success")
+
     return render_template("sign_up.html")
