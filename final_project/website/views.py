dbe5ea47feca8de9482fd122381fcb2b8c929ee5
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-03-03 00:46:47 +0100

    added remove notes functionality

--- final_project/website/views.py
+++ final_project/website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+import json
+
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 
 from .models import Note
@@ -22,3 +24,17 @@ def home():
             flash("Note added!", category="success")
 
     return render_template("home.html", user=current_user)
+
+
+@views.route("/delete-note", methods=["POST"])
+def delete_note():
+    data = json.loads(request.data)
+    note_id = data["noteId"]
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            flash("Note deleted!", category="success")
+
+    return jsonify({})
