a8dc5ce55ec11dff7c19bd686109089f478df11e
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-03-02 22:58:15 +0100

    added login functionality

--- final_project/website/auth.py
+++ final_project/website/auth.py
@@ -11,6 +11,20 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully!", category="success")
+                return redirect(url_for("views.home"))
+            else:
+                flash("Incorrect credentials", category="error")
+        else:
+            flash("Incorrect credentials", category="error")
+
     return render_template("login.html")
 
 
@@ -28,8 +42,10 @@ def sign_up():
         password_2 = request.form.get("password-2")
 
         regex = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
-
-        if not re.search(regex, email):
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exists.", category="error")
+        elif not re.search(regex, email):
             flash("Email must be valid.", category="error")
         elif len(first_name) < 2:
             flash("Name to short.", category="error")
