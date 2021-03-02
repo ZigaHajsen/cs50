af3c716f01eae7c5da201adc4caf2aafa2509761
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-03-02 21:30:23 +0100

    changed variable names

--- final_project/website/auth.py
+++ final_project/website/auth.py
@@ -19,19 +19,19 @@ def logout():
 def sign_up():
     if request.method == "POST":
         email = request.form.get("email")
-        firstName = request.form.get("firstName")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
+        first_name = request.form.get("first-name")
+        password_1 = request.form.get("password-1")
+        password_2 = request.form.get("password-2")
 
         regex = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
 
         if not re.search(regex, email):
             flash("Email must be valid.", category="error")
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("Name to short.", category="error")
-        elif password1 != password2:
+        elif password_1 != password_2:
             flash("Passwords do not match.", category="error")
-        elif len(password1) < 6:
+        elif len(password_1) < 6:
             flash("Password must be at least 6 characters.", category="error")
         else:
             flash("Account created!", category="success")
