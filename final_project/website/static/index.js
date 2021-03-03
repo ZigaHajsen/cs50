a9d40921a374ce5a505a9b8b7ee335f5eae166da
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-03-03 15:14:25 +0100

    removed redundant text

--- final_project/website/static/index.js
+++ final_project/website/static/index.js
@@ -2,7 +2,5 @@ function deleteNote(noteId) {
   fetch('/delete-note', {
     method: 'POST',
     body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = '/';
   });
 }
